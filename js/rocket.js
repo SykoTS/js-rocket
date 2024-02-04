588d3b7099302aa3d52f7cb698d90554097d583b
SykoTS <128877476+SykoTS@users.noreply.github.com>
2024-02-04 21:54:34 +0000

    Added CSS And Sounds

--- js/rocket.js
+++ js/rocket.js
@@ -1,27 +1,43 @@
 var timer = null;
-var countdownNumber = 10;
+var countdownNumber = 15;
 
 var changeStage = function(stage) {
     document.body.className = 'body-stage' +stage;
     clearInterval(timer);
-    countdownNumber = 10;
+    countdownNumber = 15;
     document.getElementById('countdown').innerHTML = countdownNumber;
 
     if (stage == 2) {
+        new Audio('sounds/rocket.mp3').play()
         timer = setInterval(function() {
             countdownNumber = countdownNumber-1;
             document.getElementById('countdown').innerHTML = countdownNumber;
+            
+            if (countdownNumber > 8 && countdownNumber <= 11){
+                document.getElementById('nervous').className = 'nervous show';
+            } else {
+                document.getElementById('nervous').className = 'nervous';
+            };
+
+            if (countdownNumber > 5 && countdownNumber <= 8){
+                document.getElementById('cant-wait').className = 'cant-wait show';
+            } else {
+                document.getElementById('cant-wait').className = 'cant-wait';
+            };
+            
+
+
             if (countdownNumber <= 0) {
                 changeStage(3);
             };
-        }, 500);
+        }, 1000);
     } else if (stage == 3) {
         var success = setTimeout(function()
         {
             var randomNumber = Math.round(Math.random()*10);
 
             console.log('randomNumber:', randomNumber)
-            if (randomNumber > 5) {
+            if (randomNumber > 2) {
                 changeStage(4);
                 new Audio('sounds/success.mp3').play()
             } else {
